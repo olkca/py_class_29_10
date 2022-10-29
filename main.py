6e2264154ba0e5bbea98ae8a7ab70b319bf2560a
Shapoval <olekcandrsapoval41@gmail.com>
2022-10-29 11:07:13 +0300

    Ex_3 Update

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
+from math import sqrt
 def num(a):
     try:
+        prime = True
 
+        i = 2
+        while i <= sqrt(a):
+            if a % i == 0:
+                prime = False
+                break
+            i += 1
+
+        if prime:
+            print("Просте")
+        else:
+            print("Складне")
     except Exception as e:
         print(f"Error {e}")
 
 
 def main():
     try:
-        num(5)
+        num(int(input("Num -->")))
     except Exception as e:
         print(f"Error {e}")
 main()
